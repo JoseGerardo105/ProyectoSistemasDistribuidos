cba88d426d5fdb6d5371af04eb5c8510665954fe
Jose Gerardo Gomez <josegomezlop@gmail.com>
2022-09-26 14:19:20 -0500

    Se agregaron códigos y valores de respuesta para los métodos de agregar, eliminar, actualizar y obtener estudiantes

--- index.js
+++ index.js
@@ -10,65 +10,69 @@ var mysqlConnection = mysql.createConnection({
     user: 'root',
     password: 'root123',
     database: 'university',
-   multipleStatements: true
+    multipleStatements: true
 });
 
-mysqlConnection.connect((err)=>{
-    if(!err)
-    console.log('DB connection succeded');
+mysqlConnection.connect((err, res) => {
+    console.log(mysqlConnection.state);
+    if (mysqlConnection.state == 'connected')
+        console.log('200 -  La conexion ha sido exitosa');
     else
-    console.log('DB connection failed \n Error: '+JSON.stringify(err,undefined,2));
+
+        res.send('503 - servidor no disponible');
 });
 
-app.listen(3000,()=>console.log('Express server is running at port no : 3000'));
+
+app.listen(3000, () => console.log('Express server is running at port no : 3000'));
 
 
 app.get('/students', (req, res) => {
     mysqlConnection.query('SELECT * FROM students', (err, rows, fields) => {
         if (!err)
-            res.send(rows);
-        else
-            console.log(err);
+            if (rows == 0) {
+                res.status(204).send('204 - No hay elementos para mostrar');
+                } else {
+                    res.send(rows);   
+                }
+            else
+            res.status(404).send('Error 404 - El estudiante no existe');
     })
 });
 
 
 app.get('/students/:id', (req, res) => {
     mysqlConnection.query('SELECT * FROM students WHERE id = ?', [req.params.id], (err, rows, fields) => {
-        if (!err)
+        if (rows != 0)
             res.send(rows);
         else
-            console.log(err);
+            res.status(404).send('Error 404: El estudiante no existe');
     })
 });
-
 
 
 app.delete('/students/:id', (req, res) => {
     mysqlConnection.query('DELETE FROM students WHERE id = ?', [req.params.id], (err, rows, fields) => {
-        if (!err)
-            res.send('Deleted successfully the student whit the id : ' + element[0].id);
+        if (!id)
+        res.status(404).send('Error 404 - el estudiante a eliminar no fue encontrado');
         else
-            console.log(err);
+        res.status(200).send('200 - Eliminado correctamente: ');
     })
 });
+
 
 
 app.post('/students', (req, res) => {
-    let student = req.body;
-    var sql = "SET @id = ?;SET @document_number = ?;SET @document_type = ?;SET @name = ?;SET @surname = ?;SET @state = ?; \
-    CALL StudentAddOrEdit(@id,@document_number,@document_type,@name,@surname,@state);";
-    mysqlConnection.query(sql, [student.id, student.document_number, student.document_type, student.name,student.surname,student.state], (err, rows, fields) => {
-        if (!err)
-            rows.forEach(element => {
-                if(element.constructor == Array)
-                res.send('Inserted student id : '+element[0].id);
-            });
-        else
-            console.log(err);
+    const params = req.body
+    mysqlConnection.query('INSERT INTO students SET ?', params, (err, rows) => {
+        if (!err) {
+            res.status(200).send('201 - El estudiante se ha insertado correctamente');
+        }else{
+            res.status(422).send('422 - Hay un dato faltante');
+        }
+
+
     })
 });
-
 
 app.put('/students', (req, res) => {
     let student = req.body;
@@ -76,9 +80,9 @@ app.put('/students', (req, res) => {
     CALL StudentAddOrEdit(@id,@document_number,@document_type,@name,@surname,@state);";
     mysqlConnection.query(sql, [student.id, student.document_number, student.document_type, student.name,student.surname,student.state], (err, rows, fields) => {
         if (!err)
-                res.send('Updated succesfully');
+            res.status(201).send('201 - El usuario se ha actualizado correctamente');
          else
-            console.log(err);
+            res.status(422).send('422 - Hay un dato faltante');
     })
 });
 
@@ -86,42 +90,44 @@ app.put('/students', (req, res) => {
 app.get('/subjects', (req, res) => {
     mysqlConnection.query('SELECT * FROM subjects', (err, rows, fields) => {
         if (!err)
-            res.send(rows);
+            if (rows == 0) {
+                res.status(204).send('204 - No hay elementos para mostrar');
+            } else {
+                res.send(rows);   
+            }
         else
-            console.log(err);
-    })
+        res.status(404).send('404 - La asignatura no existe');
+    }) 
 });
 
 app.get('/subjects/:id', (req, res) => {
     mysqlConnection.query('SELECT * FROM subjects WHERE id = ?', [req.params.id], (err, rows, fields) => {
-        if (!err)
+        if (rows != 0)
             res.send(rows);
         else
-            console.log(err);
+            res.status(404).send('Error 404: La materia buscada no existe');
     })
 });
 
 app.delete('/subjects/:id', (req, res) => {
     mysqlConnection.query('DELETE FROM subjects WHERE id = ?', [req.params.id], (err, rows, fields) => {
         if (!err)
-            res.send('Deleted successfully.');
+        res.status(404).send('Error 404 - la materia a eliminar no fue encontrada');
         else
-            console.log(err);
+        res.status(200).send('200 - Eliminado correctamente: ' );
     })
 });
 
 app.post('/subjects', (req, res) => {
-    let subject = req.body;
-    var sql = "SET @id = ?;SET @code = ?;SET @name = ?;SET @credits = ?;SET @quotas = ?;SET @state = ?; \
-    CALL SubjectAddOrEdit(@id,@code,@name,@credits,@quotas,@state);";
-    mysqlConnection.query(sql, [subject.id, subject.code, subject.name, subject.credits,subject.quotas,subject.state], (err, rows, fields) => {
-        if (!err)
-            rows.forEach(element => {
-                if(element.constructor == Array)
-                res.send('Inserted subject id : '+element[0].id);
-            });
-        else
-            console.log(err);
+    const params = req.body
+    mysqlConnection.query('INSERT INTO subjects SET ?', params, (err, rows) => {
+        if (err) {
+            res.status(422).send('422 - Hay un dato faltante');
+        } else {
+            res.status(201).send('201 - Materia insertada exitosamente');
+        }
+
+
     })
 });
 
@@ -130,13 +136,13 @@ app.put('/subjects', (req, res) => {
     let subject = req.body;
     var sql = "SET @id = ?;SET @code = ?;SET @name = ?;SET @credits = ?;SET @quotas = ?;SET @state = ?; \
     CALL SubjectAddOrEdit(@id,@code,@name,@credits,@quotas,@state);";
-    mysqlConnection.query(sql, [subject.id, subject.code, subject.name, subject.credits,subject.quotas,subject.state], (err, rows, fields) => {
+    mysqlConnection.query(sql, [subject.id, subject.code, subject.name, subject.credits, subject.quotas, subject.state], (err, rows, fields) => {
         if (!err)
             rows.forEach(element => {
-                if(element.constructor == Array)
-                res.send('Updated succesfully');
+                if (element.constructor == Array)
+                res.status(201).send('201 - Materia modificada exitosamente');
             });
         else
-            console.log(err);
-    })
+            res.status(422).send('422 - Hay un dato faltante');
+    })
 });
